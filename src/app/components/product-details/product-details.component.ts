69a3a6cd910cd9caefad96f7d00a312bf7fd493b
Artem Ivanov <ivanov@hostco.ru>
2023-12-12 14:03:21 +0500

    need redesign and adding admin panel

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { IProduct } from 'src/app/models/products';
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+	selector: 'app-product-details',
+	templateUrl: './product-details.component.html',
+	styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+	product!: IProduct;
+	productSubscription!: Subscription;
+	constructor(private route: ActivatedRoute) { }
 
+	ngOnInit(): void {
+		this.productSubscription = this.route.data.subscribe(data => {
+			this.product = data['product'];
+		});
+	}
 }
